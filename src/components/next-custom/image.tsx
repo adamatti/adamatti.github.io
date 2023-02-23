680e19d02d68416d7c9b009e1505eb99c91d58fa
Marcelo Adamatti <adamatti@gmail.com>
2023-02-23 16:56:26 -0300

    fix: static image

--- src/components/next-custom/image.tsx
+++ src/components/next-custom/image.tsx
@@ -1,16 +1,17 @@
-import { DetailedHTMLProps, ImgHTMLAttributes, type ReactElement } from 'react';
+import { type StaticImageData } from 'next/image';
+import { type DetailedHTMLProps, type ImgHTMLAttributes, type ReactElement } from 'react';
 // import ReactImage, { type ImageProps } from 'next/legacy/image';
 
-type ImageProps = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
+type OriginalImageProps = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
+
+type ImageProps = Omit<OriginalImageProps, 'src'> & { src: string | StaticImageData };
 
 export default function Image(props: ImageProps): ReactElement {
-  const { loading, src: oldsrc, ...imageProps } = props;
-  // some images are using static import
-  const src = props.src as any;
+  const { loading, src, ...imageProps } = props;
 
   // FIXME it shouldn't be required for nextjs
   const basePath = process.env.BASE_PATH ?? '';
-  let newSrc: string = src?.src ?? src;
+  let newSrc: string = typeof src === 'string' ? src : src?.src;
 
   if (basePath && newSrc?.startsWith('/')) {
     newSrc = `${basePath}${newSrc}`;
