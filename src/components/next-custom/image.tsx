1adb68843147ff84906b0bc68cf16ec31b9733b3
Marcelo Adamatti <adamatti@gmail.com>
2023-02-23 16:48:01 -0300

    fix: image usage

--- src/components/next-custom/image.tsx
+++ src/components/next-custom/image.tsx
@@ -1,13 +1,16 @@
-import { type DetailedHTMLProps, type ImgHTMLAttributes, type ReactElement } from 'react';
+import { DetailedHTMLProps, ImgHTMLAttributes, type ReactElement } from 'react';
 // import ReactImage, { type ImageProps } from 'next/legacy/image';
 
 type ImageProps = DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
 
 export default function Image(props: ImageProps): ReactElement {
-  const { loading, src, ...imageProps } = props;
-  // FIXME it shouldn't be required
+  const { loading, src: oldsrc, ...imageProps } = props;
+  // some images are using static import
+  const src = props.src as any;
+
+  // FIXME it shouldn't be required for nextjs
   const basePath = process.env.BASE_PATH ?? '';
-  let newSrc = src;
+  let newSrc: string = src?.src ?? src;
 
   if (basePath && newSrc?.startsWith('/')) {
     newSrc = `${basePath}${newSrc}`;
