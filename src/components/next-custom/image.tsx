13f2a28860a3ec9ca38f783ad28d7d126b475821
Marcelo Adamatti <adamatti@gmail.com>
2023-02-23 16:03:50 -0300

    chore: log invalid url

--- src/components/next-custom/image.tsx
+++ src/components/next-custom/image.tsx
@@ -6,8 +6,12 @@ export default function Image(props: ImageProps): ReactElement {
   // FIXME it shouldn't be required
   const basePath = process.env.BASE_PATH ?? '';
   let newSrc = src;
+
   if (basePath && typeof newSrc === 'string' && newSrc.startsWith('/')) {
     newSrc = `${basePath}${newSrc}`;
+  } else if (basePath) {
+    console.warn(`Invalid image src`, src);
   }
+
   return <ReactImage {...imageProps} loading="eager" src={newSrc} />;
 }
