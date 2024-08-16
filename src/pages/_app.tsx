675d2da0a00f9dd3fcec608751fa9218cff54213
Marcelo Adamatti <marcelo.adamatti@hotelengine.com>
2024-08-16 15:25:42 -0300

    fix: replace GA by GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { hotjar } from 'react-hotjar';
 import TailwindHacks from '~/components/layout/tailwind-hacks';
 import Header from '~/components/layout/header';
 import Footer from '~/components/layout/footer';
-import { GoogleAnalytics } from '@next/third-parties/google';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const title = 'Marcelo Adamatti Portfolio';
 
@@ -36,7 +36,7 @@ export default function App({ Component, pageProps }: AppProps): ReactElement {
           {!disableLayout && <Footer />}
         </div>
       </div>
-      {process.env.NODE_ENV === 'production' && <GoogleAnalytics gaId="G-HRDZ9LVQZW" />}
+      {process.env.NODE_ENV === 'production' && <GoogleTagManager gtmId="G-HRDZ9LVQZW" />}
     </>
   );
 }
