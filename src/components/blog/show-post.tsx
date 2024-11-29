4c6a64b8a1868dc7ce6edd140110bf86da8f25b0
Marcelo Adamatti <adamatti@gmail.com>
2024-11-29 14:13:53 -0300

    fix: lint

--- src/components/blog/show-post.tsx
+++ src/components/blog/show-post.tsx
@@ -6,11 +6,9 @@ import { type BlogPost } from '~/types';
 import BlogComments from './comments';
 import ShowTags from './show-tags';
 
-
 export default function ShowPost({ post }: { post: BlogPost }): ReactElement {
   return (
     <>
-      
       <h1 className="font-bold text-2xl">{post.title}</h1>
       <p className="text-gray-500 text-sm">
         {post.dateString}
@@ -18,9 +16,7 @@ export default function ShowPost({ post }: { post: BlogPost }): ReactElement {
       </p>
       <br />
       <article className="prose lg:prose-xl">
-        <Markdown>
-          {post?.content ?? ''}
-        </Markdown>
+        <Markdown>{post?.content ?? ''}</Markdown>
       </article>
       <br />
       <BlogComments post={post} />
--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,40 +1,38 @@
-import Head from "next/head";
-import React from "react";
+import Head from 'next/head';
+import React, { type ReactElement } from 'react';
 import MarkdownToJSX from 'markdown-to-jsx';
 
-function SyntaxHighlightedCode(props: React.HTMLProps<HTMLElement>) {
-    const ref = React.useRef<HTMLElement>(null);
-  
-    React.useEffect(() => {
-      if (ref.current && props.className?.includes('lang-') && window.hljs) {
-        window.hljs.highlightElement(ref.current)
-  
-        // hljs won't reprocess the element unless this attribute is removed
-        ref.current.removeAttribute('data-highlighted')
-      }
-    }, [props.className, props.children])
-  
-    return <code {...props} ref={ref} />
-  }
+function SyntaxHighlightedCode(props: React.HTMLProps<HTMLElement>): ReactElement {
+  const ref = React.useRef<HTMLElement>(null);
 
-export default function Markdown ({children}: {children: string}) {
-    return (<>
-        <Head>
-            <link
-            rel="stylesheet"
-            href="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/styles/nord.min.css"
-            />
-            <script
-              src="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/highlight.min.js"
-            ></script>
-        </Head>
-        <MarkdownToJSX
-          options={{
-            overrides: {
-              code: SyntaxHighlightedCode
-            }
-          }}
-          children={children}
-        />
-    </>)
+  React.useEffect(() => {
+    if (ref.current && props.className?.includes('lang-') && window.hljs) {
+      window.hljs.highlightElement(ref.current);
+
+      // hljs won't reprocess the element unless this attribute is removed
+      ref.current.removeAttribute('data-highlighted');
+    }
+  }, [props.className, props.children]);
+
+  return <code {...props} ref={ref} />;
+}
+
+export default function Markdown({ children }: { children: string }): ReactElement {
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/styles/nord.min.css" />
+        <script src="https://unpkg.com/@highlightjs/cdn-assets@11.9.0/highlight.min.js" async></script>
+      </Head>
+      <MarkdownToJSX
+        options={{
+          overrides: {
+            code: SyntaxHighlightedCode,
+          },
+        }}
+      >
+        {children}
+      </MarkdownToJSX>
+    </>
+  );
 }
