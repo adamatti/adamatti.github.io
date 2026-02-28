10838a0b5d70b919ad1e554a16e80a749a30407b
Marcelo Adamatti <adamatti@gmail.com>
2026-02-28 00:49:22 -0300

    chore: deploy

--- _next/static/RdmuOcPwI5NAIc7SRHHvk/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog","\u002Fblog\u002Fposts","\u002Fblog\u002Fposts\u002F[slug]","\u002Fblog\u002Ftags","\u002Fblog\u002Ftags\u002F[tag]","\u002Fevents","\u002Fjobs","\u002Fpet-projects","\u002Fresume","\u002Ftechnologies","\u002Ftechnologies\u002F[slug]","\u002Fvideos"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
