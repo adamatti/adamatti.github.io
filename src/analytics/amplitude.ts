5ab97c180fc43d92b7ea0a1433199ced0233d0f9
Marcelo Adamatti <adamatti@gmail.com>
2026-03-01 01:29:00 -0300

    chore: amplitude replay capture

--- src/analytics/amplitude.ts
+++ src/analytics/amplitude.ts
@@ -9,7 +9,11 @@ function initAmplitude() {
     return;
   }
   if (typeof window !== 'undefined') {
-    amplitude.add(sessionReplayPlugin());
+    amplitude.add(
+      sessionReplayPlugin({
+        sampleRate: 1, // Record 100% of sessions for now, can adjust later if needed
+      })
+    );
     amplitude.init('aa5c215800ac8ce553b4d4af6af3e2a7', { autocapture: true });
   }
 }
